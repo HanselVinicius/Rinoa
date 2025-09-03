05fbc11ea08edb1cdd030227e5f1be11e6397207
Vinicius Hansel <hanelvinicius@gmail.com>
2025-09-02 21:15:40 -0300

    feat: sql alchemy

--- src/main.py
+++ src/main.py
@@ -2,8 +2,14 @@ import os
 from extract_google_sheet import extract_google_sheet
 from dotenv import load_dotenv
 
+from db import Base,engine
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
 def main():
     load_dotenv()
+    init_db()
     spreadsheet_id = os.getenv("ABSCENSES_SPREADSHEET_ID")
     range_name = "abscenses"
     extract_google_sheet(spreadsheet_id, range_name)
